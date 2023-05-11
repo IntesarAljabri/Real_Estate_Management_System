89883fe9dcee071608ed26af2db13655c10db634
IntesarAljabri <intesaraljabri99@gmail.com>
2023-05-11 11:55:25 +0400

    NEW

---
+++ Real_Estate_Management_System/src/main/java/Real_Estate_Management_System/MyProject/RealEstateManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package Real_Estate_Management_System.MyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Real_Estate_Management_System/src/test/java/Real_Estate_Management_System/MyProject/RealEstateManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Real_Estate_Management_System.MyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
